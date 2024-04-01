5e6a6c1e724f7fe8e49dbbee93da43b2e5cf8843
BuildWithData <@Buildwd>
2024-04-01 18:25:13 +0200

    code

---
+++ src/runner/db/holdings_btc_bfill.py
@@ -0,0 +1,75 @@
+import argparse
+import logging
+import pandas as pd
+import sqlite3
+import sys
+from utils.config import CONSUMPTION_SCHEMA_PATH
+from utils.constants import TICKERS
+import warnings
+
+
+warnings.simplefilter(action='ignore', category=FutureWarning) # fillna
+
+LOGGER = logging.getLogger()
+s_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+s_handler.setFormatter(formatter)
+LOGGER.setLevel(logging.INFO)
+LOGGER.addHandler(s_handler)
+
+parser = argparse.ArgumentParser(description="update table holdings_btc_bfill")
+parser.add_argument("-d", "--date", help="target date", required=False)
+
+conn = sqlite3.connect(CONSUMPTION_SCHEMA_PATH)
+c = conn.cursor()
+
+################
+# INPUTS
+args = parser.parse_args()
+ref_date = args.date
+
+################
+# READ
+query = "select * from holdings_btc"
+
+if ref_date is not None:
+    query += f" where ref_date = '{ref_date}'"
+
+data = list(c.execute(query))
+# TODO: this should be dynamic
+df = pd.DataFrame(data, columns=["ref_date"] + TICKERS)
+
+################
+# PARSE
+out = df.fillna(method="ffill")
+out["TOTAL"] = out.iloc[:, 1:].sum(axis=1)
+
+if out.empty:
+    LOGGER.warning(f"Found no data for {ref_date}")
+    sys.exit(1)
+
+################
+# LOAD
+for row in out.itertuples():
+
+    INSERT_QUERY = "INSERT INTO holdings_btc_bfill VALUES ("
+    INSERT_QUERY += f"'{row[1]}'"
+
+    for v in row[2:]:
+        INSERT_QUERY += ","
+        if pd.isna(v):
+            v = 'Null'
+        INSERT_QUERY += f"{v}"
+
+    INSERT_QUERY += ")"
+
+    try:
+        c.execute(INSERT_QUERY)
+        LOGGER.info(f"Written data for {row[1]}")
+
+    except sqlite3.IntegrityError:
+        LOGGER.error(f"Data for {row[1]} already found in table")
+
+
+conn.commit()
+conn.close()
---
+++ src/runner/db/inflows_btc_sma5.py
@@ -0,0 +1,56 @@
+import logging
+import pandas as pd
+import sqlite3
+from utils.config import CONSUMPTION_SCHEMA_PATH
+from utils.constants import TICKERS_NO_BTCO as TICKERS
+
+
+LOGGER = logging.getLogger()
+s_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+s_handler.setFormatter(formatter)
+LOGGER.setLevel(logging.INFO)
+LOGGER.addHandler(s_handler)
+
+conn = sqlite3.connect(CONSUMPTION_SCHEMA_PATH)
+c = conn.cursor()
+
+#################
+# READ
+QUERY = "select * from inflows_btc_bxfill"
+df = pd.DataFrame(c.execute(QUERY), columns=["ref_date"] + TICKERS + ["TOTAL"])
+
+#################
+# PARSE
+out = df.fillna(0) # btmx data missing on 2024-01-15, 2024-02-19
+
+for col in TICKERS:
+    out[col] = out[col].rolling(5).mean().round(2)
+
+out["TOTAL"] = out.TOTAL.rolling(5).mean().round(2)
+
+##################
+# LOAD
+
+CLEAN_TABLE_QUERY = "DELETE FROM inflows_btc_sma5"
+c.execute(CLEAN_TABLE_QUERY)
+
+for row in out.itertuples():
+
+    INSERT_QUERY = "INSERT INTO inflows_btc_sma5 VALUES ("
+    INSERT_QUERY += f"'{row[1]}'"
+
+    for v in row[2:]:
+        INSERT_QUERY += ","
+        if pd.isna(v):
+            v = 'Null'
+        INSERT_QUERY += f"{v}"
+
+    INSERT_QUERY += ")"
+
+    c.execute(INSERT_QUERY)
+
+LOGGER.info(f"Updated data at {row[1]}")
+
+conn.commit()
+conn.close()
