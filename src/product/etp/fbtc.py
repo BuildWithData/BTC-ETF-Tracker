157dfabfa2a09ae0c3dad01f6dd4555922471f5a
BWD <114258829+BuildWithData@users.noreply.github.com>
2024-04-14 15:29:47 +0200

    [BUG]: MONTH2NUMBER missing values from april onwards

--- src/product/etp/fbtc.py
+++ src/product/etp/fbtc.py
@@ -15,7 +15,16 @@ from typing import Union
 MONTH2NUMBER = {
     "JAN": "01",
     "FEB": "02",
-    "MAR": "03"
+    "MAR": "03",
+    "APR": "04",
+    "MAY": "05",
+    "JUN": "06",
+    "JUL": "07",
+    "AUG": "08",
+    "SEP": "09",
+    "OCT": "10",
+    "NOV": "11",
+    "DEC": "12"
 }
 
 
