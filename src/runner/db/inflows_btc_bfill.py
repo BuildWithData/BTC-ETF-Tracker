29289d1291aff82f85f0b411bc7446cacc97af0f
BuildWithData <@Buildwd>
2024-04-14 18:11:25 +0200

    inflows_bfill

--- src/runner/db/inflows_btc_bfill.py
+++ src/runner/db/inflows_btc_bfill.py
@@ -30,15 +30,17 @@ QUERY = "select * from holdings_btc_bfill"
 # READ
 extracted = pd.DataFrame(
     c.execute(QUERY),
-    columns=["ref_date"] + TICKERS + ["TOTAL"],
+    columns=["ref_date", "week", "day"] + TICKERS + ["TOTAL"],
 )
 
 ##################
 # PARSE
-out = (extracted.iloc[:, 1:] - extracted.iloc[:, 1:].shift(1)).round(2)
-out["ref_date"] = extracted[["ref_date"]]
+out = (extracted.iloc[:, 3:] - extracted.iloc[:, 3:].shift(1)).round(2)
+out["ref_date"] = extracted.ref_date
+out["week"] = extracted.week
+out["day"] = extracted.day
 out = out[out["ref_date"] > "2024-02-23"]
-out = out[["ref_date"] + TICKERS + ["TOTAL"]]
+out = out[["ref_date", "week", "day"] + TICKERS + ["TOTAL"]]
 
 if ref_date is not None:
     out = out[out["ref_date"] == ref_date]
@@ -48,9 +50,9 @@ if ref_date is not None:
 for row in out.itertuples():
 
     INSERT_QUERY = "INSERT INTO inflows_btc_bfill VALUES ("
-    INSERT_QUERY += f"'{row[1]}'"
+    INSERT_QUERY += f"'{row[1]}', '{row[2]}', '{row[3]}'"
 
-    for v in row[2:]:
+    for v in row[4:]:
         INSERT_QUERY += ","
         if pd.isna(v):
             v = 'Null'
