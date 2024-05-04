3d4a0287f00e81ce030e9c7fc1eaac051fe88c19
BuildWithData <@Buildwd>
2024-05-04 09:53:31 +0200

    code

--- src/product/hk/harvest/btc.py
+++ src/product/hk/harvest/btc.py
@@ -23,7 +23,7 @@ class HB9439(ETP):
 
         options = Options()
         #options.add_argument('--headless')
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(options)
 
         driver.get(self.url())
         sleep(10)
@@ -42,8 +42,15 @@ class HB9439(ETP):
         sleep(5)
         driver.find_elements(By.CLASS_NAME, "row")[0].find_elements(By.TAG_NAME, "span")[3].click()
 
+        sleep(5)
         section = driver.find_element(By.ID, "fundvirtualasset-etf").find_element(By.XPATH, '..')
         section.find_elements(By.TAG_NAME, "span")[1].click()
+
+        sleep(5)
+        driver.execute_script("window.scrollTo(0, 1500)")
+        market_information = [t for t in driver.find_elements(By.TAG_NAME, 'h3') if "market information" in t.text.lower()][0]
+        usd_button = [s for s in market_information.find_elements(By.TAG_NAME, "span") if "USD" in s.text][0]
+        usd_button.click()
 
         sleep(5)
         path = os.path.join(self.path(), self._file_name(timestamp))
