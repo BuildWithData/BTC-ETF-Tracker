3d0961c004b14e9d145f8a01c156c10077e67d78
BWD <114258829+BuildWithData@users.noreply.github.com>
2024-05-05 14:59:59 +0200

    [FEATURE]: add scraper for HARVEST ETFs

--- src/product/hk/__init__.py
+++ src/product/hk/__init__.py
@@ -1,14 +1,19 @@
 from product.hk.bosera.btc import BB9008
 from product.hk.bosera.eth import BE9009
+from product.hk.harvest.btc import HB9439
+from product.hk.harvest.eth import HE9179
+
 
 BTC_TICKERS = [
 
-    BB9008
+    BB9008,
+    HB9439
 
 ]
 
 ETH_TICKERS = [
 
-    BE9009
+    BE9009,
+    HE9179
 
 ]
