0e5ea7302bb740a47a1e8996ee473cf128500b0f
BuildWithData <@Buildwd>
2024-05-05 15:06:56 +0200

    init

--- src/product/hk/__init__.py
+++ src/product/hk/__init__.py
@@ -1,3 +1,5 @@
+from product.hk.amc.btc import AB9042
+from product.hk.amc.eth import AE9046
 from product.hk.bosera.btc import BB9008
 from product.hk.bosera.eth import BE9009
 from product.hk.harvest.btc import HB9439
@@ -6,6 +8,7 @@ from product.hk.harvest.eth import HE9179
 
 BTC_TICKERS = [
 
+    AB9042,
     BB9008,
     HB9439
 
@@ -13,6 +16,7 @@ BTC_TICKERS = [
 
 ETH_TICKERS = [
 
+    AE9046,
     BE9009,
     HE9179
 
