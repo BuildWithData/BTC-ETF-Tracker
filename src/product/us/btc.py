3e7676dce21095d4a13d4196814d79d7c069bea2
H-Blues <hblues419@gmail.com>
2024-08-12 23:38:20 +0100

    [FEATURE]: add scraper for Grayscale BTC Mini ETFs

--- src/product/us/btc.py
+++ src/product/us/btc.py
@@ -1,5 +1,7 @@
-from datetime import datetime
+from bs4 import BeautifulSoup
+from datetime import date, datetime
 import os
+import pandas as pd
 from pathlib import Path
 from product.abc import ETP
 import requests
@@ -32,7 +34,59 @@ class BTC(ETP):
             f.write(response.text)
 
     def extract(self):
-        raise NotImplementedError
+
+        for name, content in self.files.items():
+            t = BeautifulSoup(content, "html.parser")
+
+            # Key Fund Information
+            key_fund_information = t.find_all(class_="Tables_Tables__container_table__Tw2_H")[-1]
+
+            if self.date < date.fromisoformat("2024-08-14"):
+                ref_date = key_fund_information.find(class_="Text_Text__xt7Jy Text_Text_body-small__79DVN").text.split(" ")[-1]
+            else:
+                ref_date = key_fund_information.find(class_="DownloadWithDate_Root__xBAtE DownloadWithDate_Root_white__Zr0dU").text.split(" ")[-1]
+
+            ref_date_key_fund_information = datetime.strptime(ref_date, "%d.%m.%Y").date().isoformat()
+
+            aum = float(key_fund_information.find_all(class_="TableItem_TableItem__b9eHf")[0].text.split("$")[-1].replace(",", ""))
+            n_shares = int(key_fund_information.find_all(class_="TableItem_TableItem__b9eHf")[2].text.split("g")[-1].replace(",", ""))
+            n_coins = float(key_fund_information.find_all(class_="TableItem_TableItem__b9eHf")[6].text.split("t")[-1].replace(",", ""))
+
+            # Daily Performance
+            ref_date_daily_performance = t.find(class_="DownloadWithDate_Root__xBAtE TablesContentWithHeader_Table__headDownload__L_sjH DownloadWithDate_Root_black__nxdup").text.split(" ")[-1]
+            ref_date_daily_performance = datetime.strptime(ref_date_daily_performance, "%m/%d/%Y").date().isoformat()
+            performance = t.find_all(class_="Table_Table__list__vmk_3")[2]
+
+            market_price = float(performance.find_all(class_="Text_Text__xt7Jy Text_Text_body__t0eXi TableItem_TableItem__value___2Fjx")[6].text.strip("$"))
+            daily_share_volume_traded = int(performance.find_all(class_="Text_Text__xt7Jy Text_Text_body__t0eXi TableItem_TableItem__value___2Fjx")[3].text.replace(",", ""))
+
+            self.extracted[name] = {
+               "file_name": name,
+               "ref_date_key_fund_information": ref_date_key_fund_information,
+               "market_cap": aum,
+               "n_shares": n_shares,
+               "n_coins": n_coins,
+               "ref_date_daily_performance": ref_date_daily_performance,
+               "market_price": market_price,
+               "daily_share_volume_traded": daily_share_volume_traded
+            }
 
     def update_db(self, con: Connection) -> None:
-        raise NotImplementedError
+        df = pd.DataFrame(self.extracted.values())
+
+        ##################
+        key_fund_information = df[["file_name"] + list(df.columns[1:5])]
+        key_fund_information = key_fund_information.rename({"ref_date_key_fund_information": "ref_date"}, axis=1)
+        table = "btc_key_fund_information"
+        keys = "ref_date"
+
+        self._dump(key_fund_information, table, keys, con)
+
+        ##################
+        daily_performance = df[["file_name"] + list(df.columns[5:])]
+        daily_performance = daily_performance.rename({"ref_date_daily_performance": "ref_date"}, axis=1)
+        table = "btc_daily_performance"
+        keys = "ref_date"
+
+        self._dump(daily_performance, table, keys, con)
+
